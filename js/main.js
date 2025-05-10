266a313e84dec5a67de8ae4bb9ffc604f4de0c49
Manik Gupta <manikgupta254@gmail.com>
2025-05-10 18:08:07 +0530

    Added about us page

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ window.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => loader.remove(), 100);
    }, 1300);
 });
+
+// Back to top scroll
+document.addEventListener("DOMContentLoaded", () => {
+   const backToTop = document.getElementById("back-to-top");
+
+   window.addEventListener("scroll", () => {
+      if (window.scrollY > 250) {
+         backToTop.classList.remove("d-none");
+      } else {
+         backToTop.classList.add("d-none");
+      }
+   });
+
+   backToTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+   });
+});
